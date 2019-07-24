6477458632412b793d83510d96b5bb61fea0791e
BotMaster3000 <jdgebauer@gmx.de>
2019-07-24 21:03:21 +0200

    Initial Commit

---
+++ GameOfBotHeroesVsMonsters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameOfBotHeroesVsMonsters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
