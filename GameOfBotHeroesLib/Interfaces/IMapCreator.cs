68f0cbe8ed9daafdf8ce9f091596c6f8cbb3a690
BotMaster3000 <jdgebauer@gmx.de>
2019-07-26 21:21:24 +0200

    Added IMapCreator and Implementation

---
+++ GameOfBotHeroesLib/Interfaces/IMapCreator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameOfBotLib.Enums;
+
+namespace GameOfBotLib.Interfaces
+{
+    public interface IMapCreator
+    {
+        IMap GenerateFlatMap(IDictionary<TileValues, int> tilevalueAndAppearanceChance);
+        void GenerateMapElements(IMap map);
+    }
+}
---
+++ GameOfBotHeroesLib/Logic/MapCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameOfBotLib.Enums;
+using GameOfBotLib.Interfaces;
+
+namespace GameOfBotLib.Logic
+{
+    public class MapCreator : IMapCreator
+    {
+        public IMap GenerateFlatMap(IDictionary<TileValues, int> tilevalueAndAppearanceChance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GenerateMapElements(IMap map)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
