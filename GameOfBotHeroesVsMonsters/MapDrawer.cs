31d2a46997f075b35e25154a8ccf41d69d2dd007
BotMaster3000 <jdgebauer@gmx.de>
2019-07-27 17:00:21 +0200

    Added some Logic to the GameOfBotHeroesVsMonsters-Project to display the map

---
+++ GameOfBotHeroesVsMonsters/MapDrawer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameOfBotLib.Interfaces;
+using GameOfBotLib.Enums;
+
+namespace GameOfBotHeroesVsMonsters
+{
+    public static class MapDrawer
+    {
+        public static void DrawMap(IMap map)
+        {
+            ConsoleColor previousConsoleColor = Console.ForegroundColor;
+            foreach(ITile tile in map.Tiles)
+            {
+                Console.CursorLeft = tile.XPos;
+                Console.CursorTop = tile.YPos;
+                Console.ForegroundColor = GetConsoleColorForTileValue(tile.TileValue);
+                Console.Write('X');
+            }
+            Console.ForegroundColor = previousConsoleColor;
+        }
+
+        private static ConsoleColor GetConsoleColorForTileValue(TileValues tileValue)
+        {
+            return tileValue switch
+            {
+                TileValues.Forest => ConsoleColor.DarkGreen,
+                TileValues.Grassland => ConsoleColor.DarkYellow,
+                TileValues.Fortress => ConsoleColor.DarkGray,
+                TileValues.City => ConsoleColor.DarkBlue,
+                TileValues.Village => ConsoleColor.DarkRed,
+                _ => ConsoleColor.White,
+            };
+        }
+    }
+}
--- GameOfBotHeroesVsMonsters/Program.cs
+++ GameOfBotHeroesVsMonsters/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using GameOfBotLib.Logic;
+using GameOfBotLib.Enums;
+using GameOfBotLib.Interfaces;
 
 namespace GameOfBotHeroesVsMonsters
 {
@@ -6,6 +10,22 @@ namespace GameOfBotHeroesVsMonsters
     {
         private static void Main()
         {
+            const int MapWidth = 10;
+            const int MapHeight = 10;
+            Dictionary<TileValues, int> tileValuesAndOccuranceChance = new Dictionary<TileValues, int>
+            {
+                { TileValues.City, 20 },
+                { TileValues.Forest, 250 },
+                { TileValues.Fortress, 5 },
+                { TileValues.Grassland, 500 },
+                { TileValues.Village, 50 },
+            };
+
+            MapCreator creator = new MapCreator();
+            IMap map = creator.GenerateFlatMap(MapWidth, MapHeight, tileValuesAndOccuranceChance);
+
+            MapDrawer.DrawMap(map);
+
             Console.WriteLine("Hello World!");
         }
     }
